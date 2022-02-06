aede3dc9063118a95734f1d0e9abe0a64fc651ad
tray <tray@blackbox>
2022-02-05 20:16:34 -0500

    Fixed map issues

--- unimeet/src/pages/Dashboard.js
+++ unimeet/src/pages/Dashboard.js
@@ -3,12 +3,22 @@ import { auth } from "../components/Firebase";
 import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Form,Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+/* The lines below are for the map. I would not recommend touching :) */
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
+/*-------------------------End of Leaflet stuff------------------*/
 export const Dashboard = () => {
-  
+
   const navigate = useNavigate();
   const [userdata] = useAuthState(auth);
   useEffect(() => {
@@ -21,16 +31,13 @@ export const Dashboard = () => {
       <div></div>
     )
 
-  },[userdata])
+  }, [userdata])
 
   return (
     <div className="textBackground">
       <NavBar2 />
       <h1> This is the Dashboard</h1>
-      <Inputbox/>
-      <div>
-      <Mapapi />
-      </div>
+      <Inputbox />
     </div>
 
   );
@@ -38,40 +45,50 @@ export const Dashboard = () => {
 }
 
 const Inputbox = () => {
-const [userEvents,setUserEvents] = useState([]);
-return(
-<div>
-<Form onSubmit={(e)=>{e.preventDefault()}}>
-  <Form.Group  className="mb-3" controlId="formBasicEmail" >
-    <Form.Label>Input locations</Form.Label>
-    <Form.Control type="text" onSubmit={(e)=>{e.preventDefault()}} />
-  </Form.Group>
-  <Button variant="primary" type="button" onClick={(e)=>{e.preventDefault()}}>
-    Submit
-  </Button>
-  </Form>
-</div>
+  const student_union = [35.308930837902004, -80.73367559555146];
+  const [userEvents, setUserEvents] = useState([]);
+  return (
+    <div>
+      <Form onSubmit={(e) => { e.preventDefault() }}>
+        <Form.Group className="mb-3" controlId="formBasicEmail" >
+          <Form.Label>Input locations</Form.Label>
+          <Form.Control type="text" onSubmit={(e) => { e.preventDefault() }} />
+        </Form.Group>
+        <Button variant="primary" type="button" onClick={(e) => { e.preventDefault() }}>
+          Submit
+        </Button>
+      </Form>
+      <div>
+        <Mapapi markerdata={student_union} />
+      </div>
+    </div>
 
-)
+  )
 
 }
 
 
 
-const Mapapi = () => {
+const Mapapi = (props) => {
+
   return (
     <div className="leaflet-container">
-    <MapContainer
+      <MapContainer
         center={[35.307880571109386, -80.73370127156109]}
         zoom={16}
         scrollWheelZoom={false}
-        >
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker position={[props.markerdata[0],props.markerdata[1]]}>
+          <Popup>
+            Student Union
+          </Popup>
+        </Marker>
       </MapContainer>
-</div>
+    </div>
   );
 
 
