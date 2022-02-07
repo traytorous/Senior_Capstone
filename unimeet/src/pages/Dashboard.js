135b926953ab9a1534c44858f8bdfb501fa69d0d
Tray Keller <tkeller6@uncc.edu>
2022-02-07 16:10:20 -0500

    Added radio buttons, added more functionality to mapapi function

--- unimeet/src/pages/Dashboard.js
+++ unimeet/src/pages/Dashboard.js
@@ -45,8 +45,7 @@ export const Dashboard = () => {
 }
 
 const Inputbox = () => {
-  const student_union = [35.308930837902004, -80.73367559555146];
-  const [userEvents, setUserEvents] = useState([]);
+  const [userEvents, setUserEvents] = useState(null);
   return (
     <div>
       <Form onSubmit={(e) => { e.preventDefault() }}>
@@ -57,9 +56,42 @@ const Inputbox = () => {
         <Button variant="primary" type="button" onClick={(e) => { e.preventDefault() }}>
           Submit
         </Button>
+        {/* All this does is create the radio button layout using map(kinda like a foreach loop) */}
+        {['radio'].map((type) => (
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="Student Union"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="Atkins Library"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+            <Form.Check
+              inline
+              label="Cone Student Center"
+              name="group1"
+              type={type}
+              id={`inline-${type}-3`}
+            />
+             <Form.Check
+              inline
+              label="Student Activity Center"
+              name="group1"
+              type={type}
+              id={`inline-${type}-4`}
+            />
+          </div>
+        ))}
       </Form>
       <div>
-        <Mapapi markerdata={student_union} />
+        <Mapapi markerdata={userEvents} />
       </div>
     </div>
 
@@ -70,26 +102,69 @@ const Inputbox = () => {
 
 
 const Mapapi = (props) => {
+  const student_union = [35.308930837902004, -80.73367559555146];
+  const sac = [35.3065975452604, -80.73450174470625];
+  const atkins_library = [35.30583581384564, -80.73238816397159];
+  const cone = [35.30544458961595, -80.73322067039553];
 
-  return (
-    <div className="leaflet-container">
-      <MapContainer
-        center={[35.307880571109386, -80.73370127156109]}
-        zoom={16}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[props.markerdata[0],props.markerdata[1]]}>
-          <Popup>
-            Student Union
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  if (props.markerdata == null) {
+    return (
+      <div className="leaflet-container">
+        <MapContainer
+          center={[35.307880571109386, -80.73370127156109]}
+          zoom={16}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={student_union}>
+            <Popup>
+              Student Union
+            </Popup>
+          </Marker>
+          <Marker position={sac}>
+            <Popup>
+              Student Activity Center
+            </Popup>
+          </Marker>
+          <Marker position={atkins_library}>
+            <Popup>
+              Atkins Library
+            </Popup>
+          </Marker>
+          <Marker position={cone}>
+            <Popup>
+              Cone University Center
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    );
+  } else {
+    return (
+
+      <div className="leaflet-container">
+        <MapContainer
+          center={[35.307880571109386, -80.73370127156109]}
+          zoom={16}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[props.markerdata[0], props.markerdata[1]]}>
+            <Popup>
+              Student Union
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+
+    )
+  }
 
 
 }
