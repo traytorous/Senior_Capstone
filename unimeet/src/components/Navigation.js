b0537f04cd01c81e2e05f3ea1178f1cda291d45f
ebetties <89549881+ebetties@users.noreply.github.com>
2022-03-16 18:50:46 -0400

    Update Navigation.js

--- unimeet/src/components/Navigation.js
+++ unimeet/src/components/Navigation.js
@@ -1,16 +1,21 @@
 
+
+
 import {Nav,Container,Navbar,Button } from 'react-bootstrap';
 import {signInGoogle,signOutGoogle,Userphoto} from './Firebase';
 import { useNavigate } from "react-router-dom";
+import logo from "..//images/logo.jpg";
+import uncc2 from "..//images/uncc2.jpg";
 
-/*import '../images/unimeetlogo.png'*/
+
 /*Two navbars. If signed in they switch.*/
  
 export const NavBar1 = () => {
 return(
     <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand>UniMeet</Navbar.Brand>
+    <Navbar.Brand><img src={logo} height = {120} alt= "logo"/></Navbar.Brand>
+    <img src={uncc2} height = {150} style={{paddingRight: '80px',  }}alt= "uncc2"/>
     <Button variant="success" onClick={signInGoogle}> Sign in</Button>
     </Container>
   </Navbar>
@@ -25,20 +30,23 @@ export const NavBar2 = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand onClick={navigateDashBoard}>UniMeet</Navbar.Brand>
+        <Navbar.Brand onClick={navigateDashBoard}> <img src={logo} height = {120} alt= "logo"/></Navbar.Brand>
         <Nav className="me-auto">
+        <img src={uncc2} height = {150} style={{paddingLeft: '250px',  }}alt= "uncc2"/>
         </Nav>
         <div className="custonbuttom">
           
-        <Button variant="success" onClick={navigateCreateEventPate}> Create Event</Button>
+        <Button variant="success" onClick={navigateCreateEventPate}> Create Event</Button>&nbsp;&nbsp;&nbsp;
         <Button variant="success" onClick={signOutGoogle}> Sign out</Button>
         </div>
        <Userphoto/>
+      
       </Container>
     </Navbar>
 
   )
 }
+
 
 
 
