33c2e76fed9abd3b53aa8fecc43d4331ab2f3728
Kaitlin Ennis <kennis8@uncc.edu>
2022-03-30 21:13:23 -0400

    Add basic template to the new My Events page

--- unimeet/src/pages/MyEventsPage.js
+++ unimeet/src/pages/MyEventsPage.js
@@ -1,7 +1,35 @@
+/*
+This is the My Events page - users view events they
+are hosting and/or attending.
+*/
+import { NavBar2 } from "../components/Navigation";
+import { auth } from "../components/Firebase";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+
 export const MyEventsPage = () => {
-    return (
-        <div className="textBackground">
-            <h1>My Events</h1>
-        </div>
-      )
+    const [didMount, setDidMount] = useState(false); 
+    const navigate = useNavigate();
+    const [userdata] = useAuthState(auth);
+    useEffect(() => {
+    
+      setDidMount(true);
+      if(!didMount) {
+        return null;
+      }
+    
+      if (!userdata) {
+        navigate('/')
+      }
+      
+    }, [userdata])
+    
+        return (
+            <div className="textBackground">
+            <NavBar2/>
+            <h1> Welcome {localStorage.getItem("username")} to the My Events page</h1>
+            </div>
+        );
 }
