f89aaf014e9c44cfb61a4c86013f1b812491ca88
Stephen <snetzley@uncc.edu>
2022-04-27 18:19:10 -0400

    re-updated Slideshow.jsx to not have Spongebob :)

--- unimeet/src/components/Slideshow.jsx
+++ unimeet/src/components/Slideshow.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from 'react-bootstrap';
-import Test from '../images/UNCC Logo.png';
-import mine from '../images/mine.jpg';
-import patrick from '../images/The Inner Machinations.jpg';
+import UNCC1 from '../images/unccprod1.jpg';
+import UNCC2 from '../images/unccprod2.jpg';
+import UNCC3 from '../images/unccprod3.jpg';
 
 const Slideshow = () => {
     return (
@@ -10,42 +10,42 @@ const Slideshow = () => {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={Test}
+                src={UNCC1}
                 alt="First slide"
                 width="100"
                 height="600"
                 />
                 <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h3></h3>
+                <p></p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={patrick}
+                src={UNCC3}
                 alt="Second slide"
                 width="100"
                 height="600"
                 />
 
                 <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3></h3>
+                <p></p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={mine}
+                src={UNCC2}
                 alt="Third slide"
                 width="100"
                 height="600"
                 />
 
                 <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                <h3></h3>
+                <p></p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
